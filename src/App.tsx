ca0f839d8b0a840c3d6ebde480016c7a05cd9b42
walidmuhammed <65099555+walidmuhammed@users.noreply.github.com>
2025-07-24 12:15:20 +0300

    Set up complete React application

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Mindweave</h1>
+        <p>Smart Business Management Platform</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-// You might want to import your main App component and global CSS here later
-// import App from './App.tsx';
-// import './index.css';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* Replace this with your main App component once created */}
-    <div>Loading Application...</div>
+    <App />
   </React.StrictMode>,
 );
