6ec4b716c7f263f7a4e4c381ef9c00a5069ab070
walidmuhammed <65099555+walidmuhammed@users.noreply.github.com>
2025-07-24 12:09:16 +0300

    Create missing main.tsx entry point

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// You might want to import your main App component and global CSS here later
+// import App from './App.tsx';
+// import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* Replace this with your main App component once created */}
+    <div>Loading Application...</div>
+  </React.StrictMode>,
+);
